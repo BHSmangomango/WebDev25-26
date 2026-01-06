8761dab4a064485e07b611f7956b777b322d7438
Ethano77 <ethano77@nycstudents.net>
2026-01-06 18:56:29 +0000

    hi 3.3

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,20 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function Cats1(){
+    document.getElementById("highlight").innerHTML = "this is a orange tabby cat.";
+}
+
+function Cats2(){
+    document.getElementById("highlight").innerHTML = "This cat is looking into you.";
+}
+
+function Cats3(){
+    document.getElementById("highlight").innerHTML = "This cat is looking into your eyes.";
+}
+
+function Cats4(){
+    document.getElementById("highlight").innerHTML = "This black cat is finding his owner.";
+}
+
+function Cats5(){
+    document.getElementById("highlight").innerHTML = "This cat wants to play.";
+}

00db46a30e6428c362029c3942bb46ccd5cb170a
Ethano77 <ethano77@nycstudents.net>
2026-02-13 18:32:22 +0000

    hi 4.3

--- WebDev_Unit4/WDLesson4_3Practical/script.js
+++ WebDev_Unit4/WDLesson4_3Practical/script.js
@@ -13,4 +13,25 @@ Guideline:
 
 Use past examples, classwork and practicals to assist you in completing this practical.
 */
+function BMI(){
+    let weight = parseFloat(document.getElementById("weight").value);
+    let height = parseFloat(document.getElementById("height").value);
+    let op = document.getElementById("output");
+    let formula = ((weight) / (height**2)) * 703
+    op.innerHTML = `Your BMI is ${formula}`;
+    let filename = "";
 
+    if (formula < 18.5){
+       filename = "underweight.png";
+    }elseif(formula >= 18.5 && formula <= 24.9){
+       filename = "healthyweight.png";
+    }elseif(formula >= 25 && formula <= 29.9){
+       filename = "overweight.png";
+    }elseif(formula >= 30 && formula <= 34.9){
+       filename = "obeseweight.png";
+    }elseif(formula >= 35){
+       op.innerHTML = `extremely obese`;
+    }
+    op.innerHTML = `<img src="${filename}">`;
+    }
+}
